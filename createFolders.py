b32dc61a8804f167c6305a61d36304b20361b696
RodrigoAroeira <raroeirabr@gmail.com>
2024-02-06 18:40:14 -0300

    Refactor createFolders.py to handle existing directories and add comments to main.cpp

--- createFolders.py
+++ createFolders.py
@@ -1,12 +1,15 @@
 import os, sys
 
 if len(sys.argv) > 1:
-    last = sys.argv[1]
+    last = int(sys.argv[1])
 else:
     last = 30
 
 
 for i in range(1, last+1):
-        os.mkdir(f'day{i}')
-        with open(f'day{i}/main.cpp', 'w', encoding = 'utf-8'):
-            pass
+        if not os.path.exists(f'day{i}'):
+            os.mkdir(f'day{i}')
+        with open(f'day{i}/main.cpp', 'w', encoding = 'utf-8') as f:
+            f.write("// failed compiles: \n")
+            f.write("// failed runs: \n")	
+            f.write("// time taken: \n")
