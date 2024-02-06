c977414e3e2a4890542d4bad3906c87dd3967eb6
RodrigoAroeira <raroeirabr@gmail.com>
2024-02-06 18:14:44 -0300

    createFolders.py

--- createFolders.py
+++ createFolders.py
@@ -1,5 +1,12 @@
-import os
+import os, sys
 
-for i in range(1, 31):
+if len(sys.argv) > 1:
+    last = sys.argv[1]
+else:
+    last = 30
+
+
+for i in range(1, last+1):
+        os.mkdir(f'day{i}')
         with open(f'day{i}/main.cpp', 'w', encoding = 'utf-8'):
             pass
