a0178edef64a6dbaf9d473bdc84607c024c86c82
caios <48263050+CaioS73@users.noreply.github.com>
2024-02-06 18:37:24 -0300

    Changes by Rodrigo

--- day1/main.cpp
+++ day1/main.cpp
@@ -1,3 +1,25 @@
-//
-// Created by caios on 2/6/2024.
-//
+// failed compiles: 0
+// failed runs: 2
+// time taken: 10 minutes
+
+#include <iostream>
+
+int main(int argc, char const *argv[]){
+    std::string fala;
+
+    if (argc < 2){
+        std::cout << "Qual o seu nome?" << std::endl;
+        std::cin >> fala;
+    }
+    else{
+        fala = argv[1];
+    }
+
+    if(fala == "dolan"){
+        std::cout << fala << ", voce e chucro e fede, " << fala << "!" << std::endl;
+    }else{
+        std::cout << "Hello World!" << std::endl;
+        std::cout << "Hello, " << fala << "!" << std::endl;
+    }
+    return 0;
+}
