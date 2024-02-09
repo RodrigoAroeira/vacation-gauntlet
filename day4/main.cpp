42401fd8db034a01fb0c15855b7cda210b92fe20
caios <48263050+CaioS73@users.noreply.github.com>
2024-02-09 12:23:04 -0300

    Days 4 and 5 projects finished

--- day4/main.cpp
+++ day4/main.cpp
@@ -1,3 +1,26 @@
-// failed compiles: 
-// failed runs: 
-// time taken: 
+// failed compiles: 0
+// failed runs: 0
+// time taken: 5 minutes
+
+#include <iostream>
+
+using BIG = long long int;
+
+BIG factorial(BIG n){
+    if (n == 0 || n == 1)
+        return 1;
+
+    return n * factorial(n-1);
+}
+
+
+int main(){
+    BIG n;
+
+    std::cout << "Type in a whole number to evaluate its factorial: ";
+    std::cin >> n;
+
+
+    std::cout << n << "! = " << factorial(n) << std::endl;
+    return 0;
+}
