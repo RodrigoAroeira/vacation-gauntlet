bd5bef3f5160b51d6a2830debe1e7d1c6e5e352a
caios <48263050+CaioS73@users.noreply.github.com>
2024-02-13 07:36:21 -0300

    insertion sort seems done, w/comments

--- day07/main.cpp
+++ day07/main.cpp
@@ -13,19 +13,19 @@ void print_array(int* arr, int size){
     std::cout << "]" << "\x1b[39m" << std::endl;
 }
 
-void insertion_sort(int* arr, int size){
+void insertion_sort(int* arr, int size){ // a more concise implementation can be done
     std::cout << "Sorting (Insertion sort)" << std::endl;
     int i = 0, j = 0, hold = 0;
-    for (i = 1; i < size; ++i) {
-        hold = arr[i];
-        for (j = i-1; j >= 0; --j) {
+    for (i = 1; i < size; ++i) { //second element first
+        hold = arr[i]; //checks an element, holds it
+        for (j = i-1; j >= 0; --j) { //looks back, checks if the held element is greater
             if(arr[j] > hold){
-                arr[j+1] = arr[j];
+                arr[j+1] = arr[j]; //if it is, pushes everything to the left of held element to the right
             }else{
-                break;
+                break; //if it isn't, breaks, otherwise will keep subtracting from j
             }
         }
-        arr[j+1] = hold;
+        arr[j+1] = hold; //takes j+1 (j can and will be -1 in case of a least element of the array) and equals it to hold, inserting hold into the position j stopped comparing at
     }
 }
 
