3d5d9e9e0d267ce6862acc3079d96a85dc716f74
caios <48263050+CaioS73@users.noreply.github.com>
2024-02-15 12:38:58 -0300

    Added more class attributes and fixed insert functions

--- day08/main.cpp
+++ day08/main.cpp
@@ -4,31 +4,34 @@
 
 #include <iostream>
 
-class Node{
-public:
+class Node {
 
+private:
     int data;
-    Node* next;
-
-    Node(int data) {
-        this->data = data;
-        this->next = nullptr;
+    int size;
+    Node *prev;
+    Node *next;
+public:
+    Node(int val) {
+        data = val;
+        prev = nullptr;
+        next = nullptr;
+        size = 1;
     }
 
-    Node(){}
 
-    bool searchR(int value){
-        if(data == value)
+    bool searchR(int value) {
+        if (data == value)
             return true;
-        if(next != nullptr)
+        if (next != nullptr)
             return next->searchR(value);
         return false;
     }
 
-    bool searchI(int value){
+    bool searchI(int value) {
         Node* current = this;
 
-        while(current != nullptr){
+        while (current != nullptr) {
             if (current->data == value)
                 return true;
             current = current->next;
@@ -36,72 +39,122 @@ public:
         return false;
     }
 
-    void remove(int value){
-        return; // depois eu penso nisso fodase
-        Node* current = this->next;
-        Node* prev = this;
-        Node* hold;
-        while(current != nullptr){
+    int remove_val(int value){
+        Node* current = next;
+        while(current){
             if (current->data == value) {
-                hold = current->next;
+                current->prev->next = current->next;
+                size--;
+                delete current;
+                return 0;
             }
-            prev = next->next
-
+            current = current->next;
         }
+        return -1;
     }
 
-    void insert(int value) {
-        Node *current = this;
-        while (current->next != nullptr) {
+    int remove_pos(int pos){
+        Node* current = next;
+        int i = 0;
+        while(i<size){
+            if (i == pos-1) {
+                current->prev->next = current->next;
+                size--;
+                delete current;
+                return 0;
+            }
+            current = current->next;
+            i++;
+        }
+        return -1;
+    }
+
+    void insert_back(int value) {
+        Node* current = this;
+        while (current->next) {
             current = current->next;
         }
         current->next = new Node(value);
+        current->next->prev = current;
     }
 
-    void replace(int value, int spot){
-        Node* current = this;
-        for (int i = 0; i <= spot; i++){
+    void insert_front(int value) {
+        this->next->prev = new Node(data);
+        Node* newNode = this->next->prev;
+        newNode->next = this->next;
+        newNode->prev = this;
+        this->next = newNode;
+        data = value;
+    }
+
+    void insert_in_pos(int value, int pos) {
+
+    }
+
+    void replace(int pos, int value) {
+        Node *current = this;
+        for (int i = 1; i < pos; i++) {
             current = current->next;
         }
         current->data = value;
     }
 
+
+    void print_list() {
+        Node *current = this;
+        while (current) {
+            std::cout << current->data << "->";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+
+    int getData() const {
+        return data;
+    }
+
+    void setData(int value) {
+        data = value;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
 };
 
-void print_list(Node* head){
-    Node* current = head;
-    while (current){
-        std::cout << current->data << "->";
-        current = current->next;
-    }
-    std::cout << std::endl;
-}
-
-int get_size(Node* head){
-    int count = 0;
-    Node* current = head;
-    while (current->next != nullptr){
-        count++;
-        current = current->next;
-    }
-    return count;
-}
 
 int main(){
     Node head(0);
 
 
     for (int i = 1; i <= 10; i++){
-        head.insert(i);
+        head.insert_back(i);
     }
-    std::cout << "List size: " << get_size(&head) << std::endl;
 
-    print_list(&head);
+    std::cout << "Original list:" << std::endl;
+    head.print_list();
 
-    head.remove(3);
+    head.remove_val(3);
+    std::cout << "List after remove_val(3):" << std::endl;
+    head.print_list();
 
-    print_list(&head);
+    std::cout << "List after remove_pos(0):" << std::endl;
+    head.remove_pos(5);
+    head.print_list();
 
+    std::cout << "Insert_front(94)" << std::endl;
+    head.insert_front(94);
+    head.print_list();
+
+    for (int i = 23; i < 28; i++){
+        head.insert_front(i);
+        head.print_list();
+    }
+
+    std::cout << "Replace 2, 15" << std::endl;
+    head.replace(2,15);
+    head.print_list();
 }
 
 //struct Node{};
