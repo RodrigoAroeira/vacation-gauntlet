edae441a2241f86930452918ecd8e5c5e1e42efd
RodrigoAroeira <raroeirabr@gmail.com>
2024-10-13 21:41:20 -0300

    refactor: Easier allocation tracking

--- day20/main.cpp
+++ day20/main.cpp
@@ -1,60 +1,33 @@
-
-#include "AllocationsTracker.hpp"
-
-#include <cmath>
-#include <csetjmp>
+#include <cstddef>
 #include <cstdlib>
 #include <iostream>
-#include <new>
-#include <string>
-#include <vector>
 
-AllocationsTracker &tracker = AllocationsTracker::getInstance();
-
-// needed so structures that automatically allocate memory don't recurse or smth
-bool insideNewOperator = false;
-
-void *operator new(std::size_t size) {
-  if (insideNewOperator) {
-    return malloc(size);
-  }
-
-  insideNewOperator = true;
-
-  void *ptr = malloc(size);
-  if (!ptr) {
-    insideNewOperator = false;
-    throw std::bad_alloc();
-  }
-
-  tracker.add(ptr, size);
-
-  std::cout << "Allocated " << size << " bytes at " << ptr << std::endl;
-
-  insideNewOperator = false;
-  return ptr;
-}
+static size_t sAllocations = 0;
+static size_t sDeletions = 0;
 
 void operator delete(void *ptr) noexcept {
-  if (ptr) {
-    tracker.remove(ptr);
-    free(ptr);
-    std::cout << "Deallocated memory at " << ptr << std::endl;
-  }
+  sDeletions++;
+  free(ptr);
 }
 
-void reportLeaks() { tracker.reportLeaks(); }
+void *operator new(size_t size) {
+  sAllocations++;
+  return malloc(size);
+}
+
+void reportLeaks() {
+  std::cout << sAllocations << " allocations" << std::endl;
+  std::cout << sDeletions << " deletions" << std::endl;
+  std::cout << sAllocations - sDeletions << " memory leaks detected.\n";
+}
 
 int main(int argc, char **argv) {
-  int size = argc > 1 ? std::stoi(argv[1]) : 8;
-  std::vector<long double *> randomAllocations;
+  auto arr = new int *[15];
 
-  for (int i = 0; i < size; i++)
-    randomAllocations.push_back(new long double);
+  for (int i = 0; i < 5; i++)
+    arr[i] = new int;
+
+  delete[] arr;
 
   reportLeaks();
-
-  return 0;
-  // For some reason i'm getting segfault, probably when vector or tracker map
-  // are being deleted, but it mostly works. I'll eventually figure out
 }
