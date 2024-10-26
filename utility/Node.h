f89996010ddd7a67303976752b0c73e54fa66b94
RodrigoAroeira <raroeirabr@gmail.com>
2024-10-26 14:59:58 -0300

    Added generic Node class

---
+++ utility/Node.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
+template <typename T>
+class Node {
+public:
+  using ParamType =
+      typename std::conditional<std::is_fundamental<T>::value, T, T &>::type;
+
+  Node(ParamType val) : m_data(val), m_next(nullptr) {}
+
+  operator std::string() const { return std::to_string(m_data); }
+
+  T getData() const { return m_data; }
+
+  void setData(const ParamType val) { m_data = val; }
+
+  std::shared_ptr<Node<T>> getNext() const { return m_next; }
+
+  void setNext(std::shared_ptr<Node<T>> next) { m_next = std::move(next); }
+
+  void setNext(T nextVal) { m_next = std::make_shared<Node<T>>(nextVal); }
+
+private:
+  T m_data;
+  std::shared_ptr<Node> m_next;
+};
